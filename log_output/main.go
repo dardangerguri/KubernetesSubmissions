e35596c129a2de067ed29aa78c644cea1640d83a
Dardan <dardangerguri@outlook.com>
2026-06-27 01:48:45 +0300

    Exercise 1.1 - log output

---
+++ log_output/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	id := uuid.New().String()
+
+	fmt.Println("App started with ID:", id)
+
+	for {
+		fmt.Printf("%s: %s\n",
+			time.Now().UTC().Format(time.RFC3339Nano),
+			id,
+		)
+		time.Sleep(5 * time.Second)
+	}
+}
